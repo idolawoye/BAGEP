cdb6ce27605869eb1268bfaf860d2ca18be76c06
Idowu Olawoye <idowuolawoye@gmail.com>
2019-02-11 12:30:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@
 echo "Installing dependencies from bioconda..."
 echo "Installing Fastp"
 conda install -c bioconda fastp
-echo "Installing BCFtools"
-conda install -c bioconda bcftools
-echo "Installing seqtk"
-conda install -c bioconda seqtk
 echo "Installing snippy"
 conda install -c conda-forge -c bioconda snippy
 echo "Installing mykrobe"
