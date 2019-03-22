f5e625df374d323df8a2b6f4a18838ce8db900b3
Idowu Olawoye <idowuolawoye@gmail.com>
2019-03-22 14:18:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ echo "Installing Fastp"
 conda install -c bioconda fastp
 echo "Installing snippy"
 conda install -c conda-forge -c bioconda snippy
-echo "Installing mykrobe"
-conda install -c bioconda mykrobe
 echo "Installing snakemake"
 conda install -c bioconda snakemake
 echo "Installing IQ-TREE"
 conda install -c bioconda iqtree
+echo "Installing fasttree"
+conda install -c bioconda fasttree
 echo "Installing VCF tools"
 conda install -c bioconda vcftools
 echo "DONE."
